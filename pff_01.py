e6f0d51ca1ae138792025360c2d8fd46b1c77806
tpq-classes <admin@tpq.io>
2026-01-12 12:16:57 +0100

    Initial commit

---
+++ pff_01.py
@@ -0,0 +1,16 @@
+#
+# This is my first text file.
+# This is just an illustration.
+# Once Python is installed, we 
+# can make a Python module out of this file.
+#
+import numpy as np
+
+a = np.arange(15).reshape(5, 3)
+
+print(a)
+
+print(a ** 2)
+
+print(a ** 0.5)
+
---
+++ run_docker.sh
@@ -0,0 +1,1 @@
+docker run -ti -p 9999:9999 -h pff -v $(pwd):/root/live ubuntu:latest /bin/bash
